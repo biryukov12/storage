34fc9377e36af729b874c4946e9e2e3fc1580ad8
Nikita Biryukov <nabirukov@vtb.ru>
2022-06-04 15:17:22 +0300

    Revert "test"
    This reverts commit a797d88f39009328bbb9e824e4237c8e3ae2be79.

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,6 @@ package storage
 
 import "github.com/biryukov12/storage/internal/storage"
 
-func NewStorage(SomeArg string) *storage.Storage {
+func NewStorage() *storage.Storage {
 	return storage.NewStorage()
 }
