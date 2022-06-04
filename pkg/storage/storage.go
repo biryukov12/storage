a797d88f39009328bbb9e824e4237c8e3ae2be79
Nikita Biryukov <nabirukov@vtb.ru>
2022-06-04 15:13:04 +0300

    test

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,6 @@ package storage
 
 import "github.com/biryukov12/storage/internal/storage"
 
-func NewStorage() *storage.Storage {
+func NewStorage(SomeArg string) *storage.Storage {
 	return storage.NewStorage()
 }
